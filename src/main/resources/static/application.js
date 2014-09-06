f6e104352ef277dc4251e1e88895152f2191bd6b
blackbat13 <blackbat13@gmail.com>
2014-09-06 13:02:42 +0200

    Added ajax request for getting member info from meetup.com

--- src/main/resources/static/application.js
+++ src/main/resources/static/application.js
@@ -1,4 +1,4 @@
-function getParticipants(callback)
+function getParticipants(callback, eventId, appKey)
 {
     $.ajax({
         url: "", //TODO Add url
@@ -18,4 +18,36 @@ function getParticipants(callback)
             }
         }
     });
+}
+
+function getMemberInfo(callback, userId, appKey)
+{
+    $.ajax({
+        url: "api.meetup.com/2/member.json/",
+        cache: false,
+        type: "GET",
+        dataType: "json",
+        data: {"id": userId, "key": appKey},
+        complete: function(){},
+        success: function(data){
+            if(callback != null) {
+                callback(data);
+            }
+        },
+        error: function(data, textStatus, xhr){
+            if(callback != null) {
+                callback(false);
+            }
+        }
+    });
+}
+
+function getPhotoFromMemberInfo(memberInfo)
+{
+    var photo = Object.create(null);
+    photo.highres_link = memberInfo.photo.highres_link;
+    photo.photo_id = memberInfo.photo.photo_id;
+    photo.photo_link = memberInfo.photo.photo_link;
+    photo.thumb_link = memberInfo.photo.thumb_link;
+    return photo;
 }
