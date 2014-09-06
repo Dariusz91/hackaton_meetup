000b3f71c0161badda2e5f03215ec0940b961785
varcetti <varcetti56@gmail.com>
2014-09-06 19:00:59 +0200

    RandomizeController - Accepting prizes

--- src/main/java/pl/jug/torun/controller/RandomizeController.java
+++ src/main/java/pl/jug/torun/controller/RandomizeController.java
@@ -8,7 +8,9 @@ import pl.jug.torun.domain.Draw;
 import pl.jug.torun.domain.Participant;
 import pl.jug.torun.domain.PrizeDefinition;
 import pl.jug.torun.repository.DrawRepository;
+import pl.jug.torun.repository.ParticipantRepository;
 import pl.jug.torun.repository.PrizeDefinitionRepository;
+import pl.jug.torun.service.PrizeAcceptanceService;
 import pl.jug.torun.service.RandomizeService;
 import pl.jug.torun.utils.HeaderProvider;
 
@@ -21,10 +23,16 @@ public class RandomizeController {
     private RandomizeService randomizeService;
 
     @Autowired
+    private PrizeAcceptanceService prizeAcceptanceService;
+
+    @Autowired
     private PrizeDefinitionRepository prizeDefinitionRepository;
 
     @Autowired
     private DrawRepository drawRepository;
+
+    @Autowired
+    private ParticipantRepository participantRepository;
 
     @RequestMapping(value = "/randomize/get_participant", method = RequestMethod.POST)
     @ResponseBody
@@ -54,4 +62,35 @@ public class RandomizeController {
         return new HttpEntity<>("{\"participant_id\":" + participant.getMemberId() + "}", headers);
     }
 
+    @RequestMapping(value = "/randomize/accept", method = RequestMethod.POST)
+    @ResponseBody
+    public HttpEntity<String> acceptAward(@RequestParam Map<String, String> params) {
+        HttpHeaders headers = HeaderProvider.createHeaders();
+
+        if (!params.containsKey("prize_id") || !params.containsKey("draw_id") || !params.containsKey("participant_id")
+                || !params.containsKey("accepted")) {
+            return new HttpEntity<>("{\"error\":\"Brakuje draw_id, participant_id, accepted lub prize_id\"}", headers);
+        }
+
+        Long prizeId = Long.valueOf(params.get("prize_id"));
+        Long drawId = Long.valueOf(params.get("draw_id"));
+        Long participantId = Long.valueOf(params.get("participant_id"));
+        boolean accepted = Boolean.valueOf(params.get("accepted"));
+
+        PrizeDefinition prize = prizeDefinitionRepository.findOne(prizeId);
+        Draw draw = drawRepository.findOne(drawId);
+        Participant participant = participantRepository.findOne(participantId);
+
+        if (prize == null || draw == null || participant == null) {
+            return new HttpEntity<>("{\"error\":\"Nie znaleziono losowania, uczestnika lub nagrody\"}", headers);
+        }
+
+        if (accepted) {
+            prizeAcceptanceService.acceptParticipant(participant, draw, prize);
+        } else {
+            prizeAcceptanceService.discardParticipant(participant, draw);
+        }
+
+        return new HttpEntity<>("{\"status\": \"success\"}", headers);
+    }
 }
