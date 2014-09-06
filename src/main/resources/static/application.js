a3c046fbdd29f43f3e3421f164a7835d1ef1bafd
maciej <kuropatwamaciej@gmail.com>
2014-09-06 18:03:33 +0200

    Added loading users list and prizes list.

--- src/main/resources/static/application.js
+++ src/main/resources/static/application.js
@@ -20,6 +20,27 @@ function getParticipants(callback, eventId, appKey)
     });
 }
 
+function getPrizes(callback)
+{
+    $.ajax({
+        url: "/prizes/get/all",
+        cache: false,
+        type: "GET",
+        dataType: "json",
+        complete: function(){},
+        success: function(data){
+            if(callback != null) {
+                callback(data);
+            }
+        },
+        error: function(data, textStatus, xhr){
+            if(callback != null) {
+                callback(false);
+            }
+        }
+    });
+}
+
 function getMemberInfo(callback, userId, appKey)
 {
     $.ajax({
@@ -57,15 +78,31 @@ eventId = "201836452";
 var appKey = localStorage.getItem("appKey");
 appKey = "781d47243d1f565a64a4e7b354e6358";
 var participants = new Array();
+var prizes = new Array();
 
 function processParticipants(participantsJson)
 {
+    $("#user_list").empty();
+    participants = new Array();
     participantsJson.participants.forEach(function(participantJson){
         var participant = Object.create(null);
         participant.memberId = participantJson.member_id;
         participant.name = participantJson.name;
         participants.push(participant);
+        $("#user_list").append("<li><i class='fa-li fa fa-arrow-circle-right'></i>"+ participant.name +"</li>")
+    });
+}
 
+function processPrizes(prizesJson)
+{
+    $("#prize_list").empty();
+    prizes = new Array();
+    prizesJson.prizes.forEach(function(prizeJson){
+        var prize = Object.create(null);
+        prize.id = prizeJson.id;
+        prize.name = prizeJson.name;
+        prizes.push(prize);
+        $("#prize_list").append("<li>"+prize.name+"<span class='pull-right'><i class='fa fa-minus-square'></i>1<i class='fa fa-plus-square'></i></span></li>")
     });
 }
 
