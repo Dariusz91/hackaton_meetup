6ef726c2b846228486d77af0c9da1815eb289720
bojarski <lukaszbojarski91@gmail.com>
2014-09-06 16:48:42 +0200

    Add getting all prizes

--- src/main/java/pl/jug/torun/controller/PrizeController.java
+++ src/main/java/pl/jug/torun/controller/PrizeController.java
@@ -1,11 +1,17 @@
 package pl.jug.torun.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pl.jug.torun.domain.PrizeDefinition;
+import pl.jug.torun.repository.PrizeDefinitionRepository;
 import pl.jug.torun.service.PrizeDefinitionCreationService;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -14,7 +20,10 @@ public class PrizeController {
     @Autowired
     private PrizeDefinitionCreationService prizeDefinitionCreationService;
 
-    @RequestMapping("/prizes")
+    @Autowired
+    private PrizeDefinitionRepository prizeDefinitionRepository;
+
+    @RequestMapping("/prizes/add")
     public int createPrize(@RequestParam Map<String, String> params) {
 
         if (!params.containsKey("name")) {
@@ -25,5 +34,22 @@ public class PrizeController {
         prizeDefinitionCreationService.createPrizeDefinition(prizeName);
 
         return 200;
+    }
+
+    @RequestMapping("/prizes/get/all")
+    public String getAllPrizes() {
+        List<PrizeDefinition> prizes = prizeDefinitionRepository.findAll();
+        return convertToJson(prizes);
+    }
+
+    private String convertToJson(List<PrizeDefinition> prizes) {
+        Gson gson = new Gson();
+
+        JsonElement prizesJsonArray = gson.toJsonTree(prizes);
+
+        JsonObject result = new JsonObject();
+        result.add("prizes", prizesJsonArray);
+
+        return result.toString();
     }
 }
