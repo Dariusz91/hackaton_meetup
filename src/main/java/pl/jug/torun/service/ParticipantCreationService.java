61a44da5e0082a86fecd9097c173f7715abec5b1
varcetti <varcetti56@gmail.com>
2014-09-06 12:57:54 +0200

    Restore ParticipantCreationService

--- src/main/java/pl/jug/torun/service/ParticipantCreationService.java
+++ src/main/java/pl/jug/torun/service/ParticipantCreationService.java
@@ -1,7 +1,29 @@
 package pl.jug.torun.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.jug.torun.domain.Participant;
+import pl.jug.torun.repository.ParticipantRepository;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 public class ParticipantCreationService {
+
+    @Autowired
+    private ParticipantRepository participantRepository;
+
+    public void createParticipantsIfNotExists(List<Participant> participants) {
+        List<Participant> inDb = participantRepository.findAll();
+
+        Stream<Participant> toCreate = participants.stream().filter(p -> !inDb.contains(p));
+
+        createNonExisting(toCreate);
+    }
+
+    private void createNonExisting(Stream<Participant> toCreate) {
+        toCreate.forEach(participantRepository::save);
+    }
+
 }
